485f5ea0def3f7a60202fa740b08b3acde0450ed
Euphyllia Bierque <bierque.euphyllia@gmail.com>
2026-09-20 16:25:50 +0200

    Simplify InteractionHand

--- fidorial-api/src/main/java/fr/fidorial/world/block/interaction/InteractionHand.java
+++ fidorial-api/src/main/java/fr/fidorial/world/block/interaction/InteractionHand.java
@@ -17,15 +17,13 @@ public enum InteractionHand {
      */
     OFF_HAND;
 
-    private static final InteractionHand[] BY_ID = values();
-
     /**
      * @param id the protocol identifier, {@code 0} for the main hand
      * @return the matching hand, {@link #MAIN_HAND} for anything unknown
      * @since 0.1.0
      */
     public static InteractionHand byId(final int id) {
-        return id >= 0 && id < BY_ID.length ? BY_ID[id] : MAIN_HAND;
+        return id == 1 ? OFF_HAND : MAIN_HAND;
     }
 
     /**
